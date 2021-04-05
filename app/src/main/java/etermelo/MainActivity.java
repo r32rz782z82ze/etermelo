43d6f84d620991e1e333cccdb28ed5405824e8b4
r32rz782z82ze <75548961+r32rz782z82ze@users.noreply.github.com>
2021-04-05 12:55:57 +0200

    Initial commit

---
+++ app/src/main/java/etermelo/MainActivity.java
@@ -0,0 +1,114 @@
+package com.etermelo.etermelo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.etermelo.etermelo.adapter.CategoryAdapter;
+import com.etermelo.etermelo.adapter.DiscountedProductAdapter;
+import com.etermelo.etermelo.adapter.RecentlyViewedAdapter;
+import com.etermelo.etermelo.model.Category;
+import com.etermelo.etermelo.model.DiscountedProducts;
+import com.etermelo.etermelo.model.RecentlyViewed;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.etermelo.etermelo.R.drawable.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView discountRecyclerView, categoryRecyclerView, recentlyViewedRecycler;
+    DiscountedProductAdapter discountedProductAdapter;
+    List<DiscountedProducts> discountedProductsList;
+
+    CategoryAdapter categoryAdapter;
+    List<Category> categoryList;
+
+    RecentlyViewedAdapter recentlyViewedAdapter;
+    List<RecentlyViewed> recentlyViewedList;
+
+    TextView allCategory;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        discountRecyclerView = findViewById(R.id.discountedRecycler);
+        categoryRecyclerView = findViewById(R.id.categoryRecycler);
+        allCategory = findViewById(R.id.allCategoryImage);
+        recentlyViewedRecycler = findViewById(R.id.recently_item);
+
+
+        allCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AllCategory.class);
+                startActivity(i);
+            }
+        });
+
+        // adding data to model
+        discountedProductsList = new ArrayList<>();
+        discountedProductsList.add(new DiscountedProducts(1, discountberry));
+        discountedProductsList.add(new DiscountedProducts(2, discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(3, discountmeat));
+        discountedProductsList.add(new DiscountedProducts(4, discountberry));
+        discountedProductsList.add(new DiscountedProducts(5, discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(6, discountmeat));
+
+        // adding data to model
+        categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, ic_home_fruits));
+        categoryList.add(new Category(2, ic_home_fish));
+        categoryList.add(new Category(3, ic_home_meats));
+        categoryList.add(new Category(4, ic_home_veggies));
+        categoryList.add(new Category(5, ic_home_fruits));
+        categoryList.add(new Category(6, ic_home_fish));
+        categoryList.add(new Category(7, ic_home_meats));
+        categoryList.add(new Category(8, ic_home_veggies));
+
+        // adding data to model
+       recentlyViewedList = new ArrayList<>();
+       recentlyViewedList.add(new RecentlyViewed("Görögdinnye", "A görögdinnye magas víztartalmú, és sok rostot is tartalmaz.\n", "800 Ft", "1", "KG", card4, b4));
+       recentlyViewedList.add(new RecentlyViewed("Papaja", "A papaya gömb alakú vagy körte alakú gyümölcs, amely akár 20 hüvelyk is lehet.", "2500 Ft", "1", "KG", card3, b3));
+       recentlyViewedList.add(new RecentlyViewed("Eper", "Az eper nagyon tápláló gyümölcs, C-vitaminnal töltve.", "600 Ft", "1", "KG", card2, b1));
+       recentlyViewedList.add(new RecentlyViewed("Kiwi", "Tele tápanyagokkal, mint C-vitamin, K-vitamin, E-vitamin, folát és kálium.", "700 Ft", "1", "PC", card1, b2));
+
+        setDiscountedRecycler(discountedProductsList);
+        setCategoryRecycler(categoryList);
+       setRecentlyViewedRecycler(recentlyViewedList);
+
+    }
+
+    private void setDiscountedRecycler(List<DiscountedProducts> dataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        discountRecyclerView.setLayoutManager(layoutManager);
+        discountedProductAdapter = new DiscountedProductAdapter(this,dataList);
+        discountRecyclerView.setAdapter(discountedProductAdapter);
+    }
+
+
+    private void setCategoryRecycler(List<Category> categoryDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoryRecyclerView.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this,categoryDataList);
+        categoryRecyclerView.setAdapter(categoryAdapter);
+    }
+
+    private void setRecentlyViewedRecycler(List<RecentlyViewed> recentlyViewedDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recentlyViewedRecycler.setLayoutManager(layoutManager);
+        recentlyViewedAdapter = new RecentlyViewedAdapter(this,recentlyViewedDataList);
+        recentlyViewedRecycler.setAdapter(recentlyViewedAdapter);
+    }
+    //Now again we need to create a adapter and model class for recently viewed items.
+    // lets do it fast.
+}
